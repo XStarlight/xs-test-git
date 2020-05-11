ff08bc81ef204712ace1eede4000678f16413fe2
XStarlight <flize1997@gmail.com>
2020-05-11 13:53:39 +0300

    added 0.1 version

---
+++ precache-manifest.96e0ed0276a477eff249f6b6ee2bc3b4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5926c38d2b73ed5fed471e87cf3ca833",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e8c023f3ae4ae721a43c",
+    "url": "/static/css/main.42769aa2.chunk.css"
+  },
+  {
+    "revision": "c7b35033d26019ef86a3",
+    "url": "/static/js/2.c5d18ae6.chunk.js"
+  },
+  {
+    "revision": "eff3b4d7e4bb0087ce53139cd90bc880",
+    "url": "/static/js/2.c5d18ae6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e8c023f3ae4ae721a43c",
+    "url": "/static/js/main.fc2b55b6.chunk.js"
+  },
+  {
+    "revision": "203c0d5fb515f8b0ee0f",
+    "url": "/static/js/runtime-main.d3a0c3b4.js"
+  },
+  {
+    "revision": "6f8b0fa1ef678a5e8aa1a03e5c3fc24f",
+    "url": "/static/media/preloader.6f8b0fa1.svg"
+  }
+]);
